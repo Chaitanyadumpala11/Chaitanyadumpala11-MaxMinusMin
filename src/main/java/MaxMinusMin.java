b332e03022ee333dfa8503d49f926b1c7fad26f8
Chaitanyadumpala11 <chaitanyadumpala2003@gmail.com>
2024-03-08 19:05:52 +0530

    maxmin

--- src/main/java/MaxMinusMin.java
+++ src/main/java/MaxMinusMin.java
@@ -10,6 +10,31 @@ public class MaxMinusMin {
      * @return the max value of nums minus the min value of nums.
      */
     public int difference(List<Integer> nums){
-        return 0;
+        if (nums == null || nums.isEmpty()) {
+            throw new IllegalArgumentException("List cannot be null or empty");
+        }
+        int max = nums.get(0);
+        int min = nums.get(0);
+
+        for (int num : nums) {
+            if (num > max) {
+                max = num;
+            }
+            if (num < min) {
+                min = num;
+            }
+        }
+
+        return max - min;
+    }
+
+    public static void main(String[] args) {
+    
+        MaxMinusMin maxMinusMin = new MaxMinusMin();
+        List<Integer> nums = List.of(3, 9, 1, 7, 5);
+
+        int result = maxMinusMin.difference(nums);
+        System.out.println("The difference between the max and min values: " + result);
+       
     }
 }
